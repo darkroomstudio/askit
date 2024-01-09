7aa34dd69420489223a7deeab5c7826023737e4e
devjiwonchoi <devjiwonchoi@gmail.com>
2024-01-09 21:51:47 +0900

    Fix error handling in events API and update tsconfig.json

--- api/events.ts
+++ api/events.ts
@@ -41,6 +41,9 @@ export default async function events(
       }
     }
   } catch (error) {
-    throw new Error(error)
+    if (error instanceof Error) {
+      console.error(error.message)
+      response.status(500).send(error.message)
+    }
   }
 }
