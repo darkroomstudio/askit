19f4013dbb0cfe175452d110bed4c4a3b74e55c9
devjiwonchoi <devjiwonchoi@gmail.com>
2024-01-10 02:39:06 +0900

    Update dependencies and comment out unused code in events.ts

--- api/events.ts
+++ api/events.ts
@@ -1,12 +1,4 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { App as Slack } from '@slack/bolt'
-import { getGPTResponse, generatePromptFromThread } from '../src/openai'
-
-const { client } = new Slack({
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
-})
 
 export default async function events(
   request: VercelRequest,
@@ -16,30 +8,29 @@ export default async function events(
     const requestType = request.body.type
 
     if (requestType === 'url_verification') {
-      console.log('url_verification', request.body.challenge)
-      response.status(200).send({ challenge: request.body.challenge })
+      return response.status(200).json({ challenge: request.body.challenge })
     }
 
-    if (requestType === 'event_callback') {
-      const eventType = request.body.event.type
-      if (eventType === 'app_mention') {
-        const { channel, ts, thread_ts } = request.body.event
-        const thread = await client.conversations.replies({
-          channel,
-          ts: thread_ts ?? ts,
-          inclusive: true,
-        })
+    // if (requestType === 'event_callback') {
+    //   const eventType = request.body.event.type
+    //   if (eventType === 'app_mention') {
+    //     const { channel, ts, thread_ts } = request.body.event
+    //     const thread = await client.conversations.replies({
+    //       channel,
+    //       ts: thread_ts ?? ts,
+    //       inclusive: true,
+    //     })
 
-        const prompt = await generatePromptFromThread(thread)
-        const gptResponse = await getGPTResponse(prompt)
+    //     const prompt = await generatePromptFromThread(thread)
+    //     const gptResponse = await getGPTResponse(prompt)
 
-        await client.chat.postMessage({
-          channel,
-          thread_ts: ts,
-          text: `${gptResponse.choices[0].message.content}`,
-        })
-      }
-    }
+    //     await client.chat.postMessage({
+    //       channel,
+    //       thread_ts: ts,
+    //       text: `${gptResponse.choices[0].message.content}`,
+    //     })
+    //   }
+    // }
   } catch (error) {
     if (error instanceof Error) {
       console.error(error.message)
