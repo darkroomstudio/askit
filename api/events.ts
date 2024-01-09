2288dfcdd6364a44105f91703560640174ce7092
devjiwonchoi <devjiwonchoi@gmail.com>
2024-01-09 21:33:12 +0900

    refactor: wrap with trycatch

--- api/events.ts
+++ api/events.ts
@@ -12,31 +12,35 @@ export default async function events(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  const requestType = request.body.type
+  try {
+    const requestType = request.body.type
 
-  if (requestType === 'url_verification') {
-    console.log('url_verification', request.body.challenge)
-    response.status(200).json({ challenge: request.body.challenge })
-  }
-
-  if (requestType === 'event_callback') {
-    const eventType = request.body.event.type
-    if (eventType === 'app_mention') {
-      const { channel, ts, thread_ts } = request.body.event
-      const thread = await client.conversations.replies({
-        channel,
-        ts: thread_ts ?? ts,
-        inclusive: true,
-      })
-
-      const prompt = await generatePromptFromThread(thread)
-      const gptResponse = await getGPTResponse(prompt)
-
-      await client.chat.postMessage({
-        channel,
-        thread_ts: ts,
-        text: `${gptResponse.choices[0].message.content}`,
-      })
+    if (requestType === 'url_verification') {
+      console.log('url_verification', request.body.challenge)
+      response.status(200).json({ challenge: request.body.challenge })
     }
+
+    if (requestType === 'event_callback') {
+      const eventType = request.body.event.type
+      if (eventType === 'app_mention') {
+        const { channel, ts, thread_ts } = request.body.event
+        const thread = await client.conversations.replies({
+          channel,
+          ts: thread_ts ?? ts,
+          inclusive: true,
+        })
+
+        const prompt = await generatePromptFromThread(thread)
+        const gptResponse = await getGPTResponse(prompt)
+
+        await client.chat.postMessage({
+          channel,
+          thread_ts: ts,
+          text: `${gptResponse.choices[0].message.content}`,
+        })
+      }
+    }
+  } catch (error) {
+    console.error(error)
   }
 }
