204d191749aa9962f948a5d04d527b7af2b1d262
devjiwonchoi <devjiwonchoi@gmail.com>
2024-01-09 20:35:39 +0900

    feat: add vercel serverless function

---
+++ api/events.ts
@@ -0,0 +1,41 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { App as Slack } from '@slack/bolt'
+import { getGPTResponse, generatePromptFromThread } from '../src/openai'
+
+const { client } = new Slack({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+})
+
+export default async function events(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  const requestType = request.body.type
+
+  if (requestType === 'url_verification') {
+    return response.status(200).json({ challenge: request.body.challenge })
+  }
+
+  if (requestType === 'event_callback') {
+    const eventType = request.body.event.type
+    if (eventType === 'app_mention') {
+      const { channel, ts, thread_ts } = request.body.event
+      const thread = await client.conversations.replies({
+        channel,
+        ts: thread_ts ?? ts,
+        inclusive: true,
+      })
+
+      const prompt = await generatePromptFromThread(thread)
+      const gptResponse = await getGPTResponse(prompt)
+
+      await client.chat.postMessage({
+        channel,
+        thread_ts: ts,
+        text: `${gptResponse.choices[0].message.content}`,
+      })
+    }
+  }
+}
