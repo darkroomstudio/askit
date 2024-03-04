595388c7bb233fd7160a153ec81bfef4d8133079
Jiwon Choi <devjiwonchoi@gmail.com>
2024-03-05 01:30:35 +0900

    feat: add cron logger (#11)
    * feat: add cron logger
    
    * Revert "feat: add cron logger"
    
    This reverts commit aec7f57138dc610a49e8c740545c675f18562563.
    
    * chore: add vercel cron logger

--- api/remind/stand-up.ts
+++ api/remind/stand-up.ts
@@ -1,3 +1,4 @@
+import VercelCronLogger from 'vercel-cron-logger'
 import { sendReminder } from '../_chat'
 
 export async function GET(request: Request) {
@@ -5,6 +6,7 @@ export async function GET(request: Request) {
 
   // See https://vercel.com/docs/cron-jobs/manage-cron-jobs#securing-cron-jobs
   if (authHeader === `Bearer ${process.env.CRON_SECRET}`) {
+    await VercelCronLogger(request)
     await sendReminder('30분 뒤 스탠드업 미팅이 시작됩니다. 🤓')
     return new Response('Success!', { status: 200 })
   }
