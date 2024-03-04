e8a37dbf1b92f8818794b2e4b2affadac04047cb
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-05 01:29:54 +0900

    chore: add vercel cron logger

--- api/remind/group-up.ts
+++ api/remind/group-up.ts
@@ -1,3 +1,4 @@
+import VercelCronLogger from 'vercel-cron-logger'
 import { sendReminder } from '../_chat'
 
 export async function GET(request: Request) {
@@ -5,6 +6,7 @@ export async function GET(request: Request) {
 
   // See https://vercel.com/docs/cron-jobs/manage-cron-jobs#securing-cron-jobs
   if (authHeader === `Bearer ${process.env.CRON_SECRET}`) {
+    await VercelCronLogger(request)
     await sendReminder('30분 뒤 몰입 세션이 시작됩니다. 😌')
     return new Response('Success!', { status: 200 })
   }
